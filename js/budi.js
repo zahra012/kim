223d4920c56a03a45a8f9eebd727db5b0bb1de0b
zahra012 <zahraprajuni@yahoo.com>
2017-09-14 11:38:23 +0700

    Add files via upload

---
+++ js/budi.js
@@ -0,0 +1,364 @@
+        function a1() {           
+        document.getElementById('01').value = "01";
+        document.getElementById('angkoinduak').value ="01";
+    }
+        function a2() {           
+        document.getElementById('02').value = "02";
+        document.getElementById('angkoinduak').value ="02";
+    }
+        function a3() {           
+        document.getElementById('03').value = "03";
+        document.getElementById('angkoinduak').value ="03";
+    }
+        function a4() {           
+        document.getElementById('04').value = "04";
+        document.getElementById('angkoinduak').value ="04";
+    }
+        function a5() {           
+        document.getElementById('05').value = "05";
+        document.getElementById('angkoinduak').value ="05";
+    }
+        function a6() {           
+        document.getElementById('06').value = "06";
+        document.getElementById('angkoinduak').value ="06";
+    }
+        function a7() {           
+        document.getElementById('07').value = "07";
+        document.getElementById('angkoinduak').value ="07";
+    }
+        function a8() {           
+        document.getElementById('08').value = "08";
+        document.getElementById('angkoinduak').value ="08";
+    }
+        function a9() {           
+        document.getElementById('09').value = "09";
+        document.getElementById('angkoinduak').value ="09";
+    }
+        function a10() {              
+        document.getElementById('10').value = "10";
+        document.getElementById('angkoinduak').value ="10";
+    }
+        function b11() {              
+        document.getElementById('11').value = "11";
+        document.getElementById('angkoinduak').value ="11";
+    }
+        function b12() {              
+        document.getElementById('12').value = "12";
+        document.getElementById('angkoinduak').value ="12";
+    }
+        function b13() {              
+        document.getElementById('13').value = "13";
+        document.getElementById('angkoinduak').value ="13";
+    }
+        function b14() {              
+        document.getElementById('14').value = "14";
+        document.getElementById('angkoinduak').value ="14";
+    }
+        function b15() {              
+        document.getElementById('15').value = "15";
+        document.getElementById('angkoinduak').value ="15";
+    }
+        function b16() {              
+        document.getElementById('16').value = "16";
+        document.getElementById('angkoinduak').value ="16";
+    }
+        function b17() {              
+        document.getElementById('17').value = "17";
+        document.getElementById('angkoinduak').value ="17";
+    }
+        function b18() {              
+        document.getElementById('18').value = "18";
+        document.getElementById('angkoinduak').value ="18";
+    }
+        function b19() {              
+        document.getElementById('19').value = "19";
+        document.getElementById('angkoinduak').value ="19";
+    }
+        function b20() {              
+        document.getElementById('20').value = "20";
+        document.getElementById('angkoinduak').value ="20";
+    }
+        function c21() {              
+        document.getElementById('21').value = "21";
+        document.getElementById('angkoinduak').value ="21";
+    }
+        function c22() {              
+        document.getElementById('22').value = "22";
+        document.getElementById('angkoinduak').value ="22";
+    }
+        function c23() {              
+        document.getElementById('23').value = "23";
+        document.getElementById('angkoinduak').value ="23";
+    }
+        function c24() {              
+        document.getElementById('24').value = "24";
+        document.getElementById('angkoinduak').value ="24";
+    }
+        function c25() {              
+        document.getElementById('25').value = "25";
+        document.getElementById('angkoinduak').value ="25";
+    }
+        function c26() {              
+        document.getElementById('26').value = "26";
+        document.getElementById('angkoinduak').value ="26";
+    }
+        function c27() {              
+        document.getElementById('27').value = "27";
+        document.getElementById('angkoinduak').value ="27";
+    }
+        function c28() {              
+        document.getElementById('28').value = "28";
+        document.getElementById('angkoinduak').value ="28";
+    }
+        function c29() {              
+        document.getElementById('29').value = "29"; 
+        document.getElementById('angkoinduak').value ="29";
+    }
+        function c30() {              
+        document.getElementById('30').value = "30";
+        document.getElementById('angkoinduak').value ="30";
+    }
+        function d31() {              
+        document.getElementById('31').value = "31";
+        document.getElementById('angkoinduak').value ="31";
+    }
+        function d32() {              
+        document.getElementById('32').value = "32";
+        document.getElementById('angkoinduak').value ="32";
+    }
+        function d33() {              
+        document.getElementById('33').value = "33";
+        document.getElementById('angkoinduak').value ="33";
+    }
+        function d34() {              
+        document.getElementById('34').value = "34";
+        document.getElementById('angkoinduak').value ="34";
+    }
+        function d35() {              
+        document.getElementById('35').value = "35";
+        document.getElementById('angkoinduak').value ="35";
+    }
+        function d36() {              
+        document.getElementById('36').value = "36";
+        document.getElementById('angkoinduak').value ="36";
+    }
+        function d37() {              
+        document.getElementById('37').value = "37";
+        document.getElementById('angkoinduak').value ="37";
+    }
+        function d38() {              
+        document.getElementById('38').value = "38";
+        document.getElementById('angkoinduak').value ="38";
+    }
+        function d39() {              
+        document.getElementById('39').value = "39";
+        document.getElementById('angkoinduak').value ="39";
+    }
+        function d40() {              
+        document.getElementById('40').value = "40";
+        document.getElementById('angkoinduak').value ="40";
+    }
+        function e41() {             
+        document.getElementById('41').value = "41";
+        document.getElementById('angkoinduak').value ="41";
+    }
+        function e42() {              
+        document.getElementById('42').value = "42";
+        document.getElementById('angkoinduak').value ="42";
+    }
+        function e43() {              
+        document.getElementById('43').value = "43";
+        document.getElementById('angkoinduak').value ="43";
+    }
+        function e44() {             
+        document.getElementById('44').value = "44";
+        document.getElementById('angkoinduak').value ="44";
+    }
+        function e45() {              
+        document.getElementById('45').value = "45";
+        document.getElementById('angkoinduak').value ="45";
+    }
+        function e46() {              
+        document.getElementById('46').value = "46";
+        document.getElementById('angkoinduak').value ="46";
+    }
+        function e47() {              
+        document.getElementById('47').value = "47";
+        document.getElementById('angkoinduak').value ="47";
+    }
+        function e48() {              
+        document.getElementById('48').value = "48";
+        document.getElementById('angkoinduak').value ="48";
+    }
+        function e49() {              
+        document.getElementById('49').value = "49";
+        document.getElementById('angkoinduak').value ="49";
+    }
+        function e50() {              
+        document.getElementById('50').value = "50";
+        document.getElementById('angkoinduak').value ="50";
+    }
+        function f51() {              
+        document.getElementById('51').value = "51";
+        document.getElementById('angkoinduak').value ="51";
+    }
+        function f52() {              
+        document.getElementById('52').value = "52";
+        document.getElementById('angkoinduak').value ="52";
+    }
+        function f53() {              
+        document.getElementById('53').value = "53";
+        document.getElementById('angkoinduak').value ="53";
+    }
+        function f54() {              
+        document.getElementById('54').value = "54";
+        document.getElementById('angkoinduak').value ="54";
+    }
+        function f55() {              
+        document.getElementById('55').value = "55";
+        document.getElementById('angkoinduak').value ="55";
+    }
+        function f56() {              
+        document.getElementById('56').value = "56";
+        document.getElementById('angkoinduak').value ="56";
+    }
+        function f57() {              
+        document.getElementById('57').value = "57";
+        document.getElementById('angkoinduak').value ="57";
+    }
+        function f58() {              
+        document.getElementById('58').value = "58";
+        document.getElementById('angkoinduak').value ="58";
+    }
+        function f59() {              
+        document.getElementById('59').value = "59";
+        document.getElementById('angkoinduak').value ="59";
+    }
+        function f60() {              
+        document.getElementById('60').value = "60";
+        document.getElementById('angkoinduak').value ="60";
+    }
+        function g61() {              
+        document.getElementById('61').value = "61";
+        document.getElementById('angkoinduak').value ="61";
+    }
+        function g62() {              
+        document.getElementById('62').value = "62";
+        document.getElementById('angkoinduak').value ="62";
+    }
+        function g63() {              
+        document.getElementById('63').value = "63";
+        document.getElementById('angkoinduak').value ="63";
+    }
+        function g64() {              
+        document.getElementById('64').value = "64";
+        document.getElementById('angkoinduak').value ="64";
+    }
+        function g65() {              
+        document.getElementById('65').value = "65";
+        document.getElementById('angkoinduak').value ="65";
+    }
+        function g66() {              
+        document.getElementById('66').value = "66";
+        document.getElementById('angkoinduak').value ="66";
+    }
+        function g67() {              
+        document.getElementById('67').value = "67";
+        document.getElementById('angkoinduak').value ="67";
+    }
+        function g68() {              
+        document.getElementById('68').value = "68";
+        document.getElementById('angkoinduak').value ="68";
+    }
+        function g69() {              
+        document.getElementById('69').value = "69";
+        document.getElementById('angkoinduak').value ="69";
+    }
+        function g70() {              
+        document.getElementById('70').value = "70";
+        document.getElementById('angkoinduak').value ="70";
+    }
+        function h71() {              
+        document.getElementById('71').value = "71";
+        document.getElementById('angkoinduak').value ="71";
+    }
+        function h72() {              
+        document.getElementById('72').value = "72";
+        document.getElementById('angkoinduak').value ="72";
+    }
+        function h73() {              
+        document.getElementById('73').value = "73";
+        document.getElementById('angkoinduak').value ="73";
+    }
+        function h74() {              
+        document.getElementById('74').value = "74";
+        document.getElementById('angkoinduak').value ="74";
+    }
+        function h75() {              
+        document.getElementById('75').value = "75";
+        document.getElementById('angkoinduak').value ="75";
+    }
+        function h76() {              
+        document.getElementById('76').value = "76";
+        document.getElementById('angkoinduak').value ="76";
+    }
+        function h77() {              
+        document.getElementById('77').value = "77";
+        document.getElementById('angkoinduak').value ="77";
+    }
+        function h78() {              
+        document.getElementById('78').value = "78";
+        document.getElementById('angkoinduak').value ="78";
+    }
+        function h79() {              
+        document.getElementById('79').value = "79";
+        document.getElementById('angkoinduak').value ="79";
+    }
+        function h80() {              
+        document.getElementById('80').value = "80";
+        document.getElementById('angkoinduak').value ="80";
+    }
+        function i81() {              
+        document.getElementById('81').value = "81";
+        document.getElementById('angkoinduak').value ="81";
+    }
+        function i82() {              
+        document.getElementById('82').value = "82";
+        document.getElementById('angkoinduak').value ="82";
+    }
+        function i83() {              
+        document.getElementById('83').value = "83";
+        document.getElementById('angkoinduak').value ="83";
+    }
+        function i84() {              
+        document.getElementById('84').value = "84";
+        document.getElementById('angkoinduak').value ="84";
+    }
+        function i85() {              
+        document.getElementById('85').value = "85";
+        document.getElementById('angkoinduak').value ="85";
+    }
+        function i86() {              
+        document.getElementById('86').value = "86";
+        document.getElementById('angkoinduak').value ="86";
+    }
+        function i87() {              
+        document.getElementById('87').value = "87";
+        document.getElementById('angkoinduak').value ="87";
+    }
+        function i88() {              
+        document.getElementById('88').value = "88";
+        document.getElementById('angkoinduak').value ="88";
+    }
+        function i89() {              
+        document.getElementById('89').value = "89";
+        document.getElementById('angkoinduak').value ="89";
+    }
+        function i90() {              
+        document.getElementById('90').value = "90";
+        document.getElementById('angkoinduak').value ="90";
+    }    
+        function warna(color){
+            document.body.style.background= color;
+    }
+    <!-- var data1 = document.getbyid('angka') -->-
